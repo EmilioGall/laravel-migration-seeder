de9e751272dc98f3b381df86065e32092dc87ffd
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-11 21:49:57 +0200

    feat: added Model - Train.php & added Controller - TrainController.php & added controller link to View - home.blade.php

--- database/migrations/2024_06_11_182134_update_trains_table.php
+++ database/migrations/2024_06_11_182134_update_trains_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('trains', function (Blueprint $table) {
-            $table->smallInteger('seats_number')->unsigned()->after('cars_number');
-            // $table->time('departure_time')->unsigned()->change();
-            // $table->time('arrive_time')->unsigned()->change();
+            $table->unsignedSmallInteger('seats_number')->after('cars_number');
+            // $table->time('departure_time')->change();
+            // $table->time('arrive_time')->change();
         });
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-   <h1 class="text-center">
-
-      Laravel-Vite Template <br>
-
-      <em class="fs-4">
-         with Bootstrap <i class="fa-brands fa-bootstrap"></i> and Fontawesome <i class="fa-solid fa-font-awesome"></i>
-      </em>
-
-   </h1>
+<main>
+   
+</main>
 @endsection
