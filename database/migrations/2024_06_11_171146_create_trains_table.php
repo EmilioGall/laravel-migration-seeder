ef9db0b8b23e4b76f3f83ec6b25db55c42ab0e04
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-11 19:53:15 +0200

    feat: added methods of up function - migration - create_trains_table.php

--- database/migrations/2024_06_11_171146_create_trains_table.php
+++ database/migrations/2024_06_11_171146_create_trains_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrive_station');
+            $table->date('departure_day');
+            $table->time('departure_time');
+            $table->date('arrive_day');
+            $table->time('arrive_time');
+            $table->string('train_code');
+            $table->unsignedTinyInteger('cars_number');
+            $table->boolean('is_in_time')->default(true);
+            $table->boolean('is_deleted')->default(true);
             $table->timestamps();
         });
     }
