ac21d87f37bca4f8106c653bdae5fc2ff8bd3241
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-12 16:02:51 +0200

    feat: added fake data with Faker - TrainsTableSeeder.php

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,10 +2,13 @@
 
 namespace Database\Seeders;
 
+use Faker;
+use App\Models\Train;
+
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -14,6 +17,26 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+
+            $faker = Faker\Factory::create('it_IT');
+
+            $newTrain = new Train();
+
+            $newTrain->company = $faker->company();
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrive_station = $faker->city();
+            $newTrain->departure_day = $faker->dateTimeThisMonth('+12 days');
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrive_day = $faker->dateTimeThisMonth('+12 days');
+            $newTrain->arrive_time = $faker->time();
+            $newTrain->train_code = substr($faker->text(5), 0, -3) . $faker->randomNumber(4, true);
+            $newTrain->cars_number = $faker->numberBetween(2, 6);
+            $newTrain->seats_number = $faker->numberBetween(50, 280);
+            $newTrain->is_in_time = $faker->boolean();
+            $newTrain->is_deleted = $faker->boolean();
+
+            $newTrain->save();
+        }
     }
 }
